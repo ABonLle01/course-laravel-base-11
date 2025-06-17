3dfaffe5f792c6714eeea2808d33252c507aecd8
alvaro <alvaro.bonilla.lledo@gmail.com>
2025-06-17 10:14:43 +0200

    CRUD modelo BD

---
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $post = Post::find(1);
+
+        
+
+        /* $post->update(
+            [
+                'title' => 'test title new',
+                'slug' => 'test-slug',
+                'content' => 'test content',
+                'image' => 'image',
+            ]
+        ); */
+
+        return "Index";
+
+        /* Post::create(
+            [
+                'title' => 'test title',
+                'slug' => 'test-slug',
+                'description' => 'test description',
+                'content' => 'test content',
+                'image' => 'image',
+                'posted' => 'not',
+                'category_id' => 1,
+            ]
+        ); */
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
